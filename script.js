7e75abccb1ad403f989fea7db3270e1618441012
JakubMachac <machynkar@gmail.com>
2020-10-06 13:13:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function rofl() {
+  let jm = document.getElementById("jmeno").value;
+  console.log(jm);
+ alert("CUS" + jm);
+}
+
+
+function vypocet() {
+  let c1 = document.getElementById("cislo1").valueAsNumber;
+    let c2 = document.getElementById("cislo2").valueAsNumber;
+    let vyp = c1 + c2
+  console.log(vyp);
+}
