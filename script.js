ce98f1d5f473bcd5179d307d786801ff32433446
Jakub Machač <machynkar@gmail.com>
2020-10-06 13:43:53 +0000

    verze 2

--- script.js
+++ script.js
@@ -8,6 +8,22 @@ function rofl() {
 function vypocet() {
   let c1 = document.getElementById("cislo1").valueAsNumber;
     let c2 = document.getElementById("cislo2").valueAsNumber;
-    let vyp = c1 + c2
+    let op = document.getElementById("operace").value
+
+    let vyp = "?";
+    if (isNaN(c1)) {
+      alert("Neni zadano prvni cislo");
+    } else if (isNaN(c2)) {
+      alert("Neni zadano druhe cislo");
+    }
+    else if (op == "plus") {
+      vyp = c1 + c2;
+    }
+    else if (op == "minus") {
+      vyp = c1 - c2;
+    }
   console.log(vyp);
+  document.getElementById("vysledek").value = vyp ;
+  document.getElementById("vysledek").innerHTML = vyp ;
+  
 }
